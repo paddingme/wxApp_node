c0611a4b2e7a1954c1facc5b18dd41ba63672994
qimuyunduan <qimuyunduan@gmail.com>
2016-10-03 08:50:35 +0800

    add app routes

--- app.js
+++ app.js
@@ -1,69 +1,51 @@
-var path = require('path'),
-    restify = require('restify'),
-    fs = require('fs');
+var path            = require('path'),
+    restify         = require('restify'),
+    fs              = require('fs'),
+    mongoose        = require('mongoose'),
+    registerPlugins = require('./plugins');
 
+
+//create server
 var server = restify.createServer({
     certificate: fs.readFileSync('path/to/server/certificate'),
     key: fs.readFileSync('path/to/server/key'),
     name: 'WXApp'
 });
 
-server.listen(8080);
-//use plugins
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.authorizationParser());
-server.use(restify.dateParser());
-server.use(restify.queryParser());
-server.use(restify.jsonp());
-server.use(restify.gzipResponse());
-server.use(restify.bodyParser());
-server.use(restify.requestExpiry());
-server.use(restify.throttle({
-    burst: 100,
-    rate: 50,
-    ip: true,
-    overrides: {
-        '192.168.1.1': {
-            rate: 0,        // unlimited
-            burst: 0
-        }
-    }
-}));
-server.use(restify.conditionalRequest());
+//register server plugins
+registerPlugins(server);
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
 
 // development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
+server.listen(8080);
 
 module.exports = app;
--- plugins/index.js
+++ plugins/index.js
@@ -1,3 +1,42 @@
 /**
  * Created  on 10/2/2016.
  */
+var restify = require('restify');
+
+/**
+ * write some custom plugins for server
+ */
+//function customPlugin(){
+//
+//}
+
+
+
+/**
+ *
+ * @param server
+ */
+
+module.exports = function(server){
+    //use plugins
+    server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.authorizationParser());
+    server.use(restify.dateParser());
+    server.use(restify.queryParser());
+    server.use(restify.jsonp());
+    server.use(restify.gzipResponse());
+    server.use(restify.bodyParser());
+    server.use(restify.requestExpiry());
+    server.use(restify.throttle({
+        burst: 100,
+        rate: 50,
+        ip: true,
+        overrides: {
+            '192.168.1.1': {
+                rate: 0,        // unlimited
+                burst: 0
+            }
+        }
+    }));
+    server.use(restify.conditionalRequest());
+};
