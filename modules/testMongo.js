af80841e6356aceade0ecef60353d81b49ebe123
qimuyunduan <mm101410>
2016-10-13 17:25:22 +0800

    db test

---
+++ modules/testMongo.js
@@ -0,0 +1,20 @@
+/**
+ * Created by bas on 2016/10/11.
+ */
+
+var userModel = require('../db/index').getMongoConnection().model("user");
+userModel.create({
+    openId: "fewrgrefgresf",
+    nickName: "test",
+    gender: "男",
+    city: "zhenjiang",
+    province: "江苏",
+    country: "china",
+    avatarUrl: "xsdsdsdsdsa",
+    unionId: "cfgegfefe",
+    flag: 1
+},function (err) {
+    if(!err){
+        console.log("创建数据成功");
+    }
+});
---
+++ modules/testRedis.js
@@ -0,0 +1,5 @@
+/**
+ * Created by bas on 2016/10/11.
+ */
+
+var redisClient = require("./redis");
